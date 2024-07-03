8ee31f2a566e78f5e26e0035286445cedda0f86e
Assumpta <assumptanalubowa@gmail.com>
2024-07-03 12:39:10 +0300

    Creates a single green dot on the screen

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function testScript() {
-    alert("The Javascript works")
-}
+// function randomColorValue() {
+//     return Math.floor(Math.random() * 256)
+// }
+
+// function randomColor() {
+//     return `rgb(${randomColorValue()}, ${randomColorValue()}, ${randomColorValue()})`
+// }
+
+// function paintNewDot(clickEvent) {
+//     const dot = document.createElement("div")
+//     dot.className = "dot"
+//     dot.style.left = clickEvent.pageX - 7 + "px"
+//     dot.style.top = clickEvent.pageY - 7  + "px"
+//     dot.style.backgroundColor = randomColor()
+//     canvas.appendChild(dot)
+//     console.log(clickEvent)
+// }
+
+// const canvas = document.getElementById("canvas")
+// canvas.addEventListener("click", paintNewDot)
